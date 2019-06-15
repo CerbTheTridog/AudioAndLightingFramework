6fc72d77427e39a1594d7193c9d2fdad57f612e1
root <sidewayslion@gmail.com>
2019-06-15 14:56:25 -0700

    small timing bug fix to control

--- cl_lights/cl_lights.h
+++ cl_lights/cl_lights.h
@@ -5,8 +5,8 @@
 #define CONTROL_PI_IP "127.0.0.1"
 #define CONTROL_PI_ACC_PORT 15556
 #define LED_ARRAY_LEN 5
-#define COLOR_GEN_DELAY 300000 /* microseconds */
-#define SEND_DELAY 400000 /* microseconds */
+#define COLOR_GEN_DELAY 800000 /* microseconds */
+#define SEND_DELAY 40000 /* microseconds */
 #define UINT32_FULL_RED 255
 #define UINT32_FULL_GREEN 255
 #define UINT32_FULL_BLUE 255
